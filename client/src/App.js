5875f5a61c4294a8f1fb384d25b07d3092083bf6
doandu <doandu@oregonstate.edu>
2022-10-12 20:12:57 -0700

    added hamburger menu to improve website functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+
 import Header from './Header';
+import HamMenu from './Menu';
+
 function App() {
   return (
     <div className="App">
+      <HamMenu/>
       <Header/>
       <div className='main'>
         stuff goes here
