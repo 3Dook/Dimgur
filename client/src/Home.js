1acff3a467357b12f32fb5fa4168b621f741ac32
doandu <doandu@oregonstate.edu>
2023-01-05 18:14:11 -0800

    updated Home page to incorporate search features and set structure for loading images cards

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,8 +1,31 @@
+import React, {useState} from 'react'
 
 function Home() {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearchChange = (e) =>{
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  }
+
+  // method to use the update search input to find images or details
+
   return (
     <div className="">
         Home
+      <div>
+        <input 
+          className='searchBar'
+          type="text"
+          placeholder='Search Here'
+          onChange={handleSearchChange}
+          value={searchInput}
+        />
+        <div>{searchInput}</div>
+      </div>
+      <div>
+        Images content cards go here
+      </div>
     </div>
   );
 }
